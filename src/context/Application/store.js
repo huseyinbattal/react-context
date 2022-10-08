43e36e770a2ccafa3c0b56f7ce9e7dad8207086a
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-10-08 20:46:53 +0300

    prettier

--- src/context/Application/store.js
+++ src/context/Application/store.js
@@ -1,22 +1,21 @@
-import {createContext, useReducer} from 'react'
-import appReducer from './reducer'
-import {initialState} from "./initalState";
+import { createContext, useReducer } from "react";
+import appReducer from "./reducer";
+import { initialState } from "./initalState";
 import useAppDispatch from "./useAppDispatch";
 import useAppActions from "./useAppActions";
 import useAppState from "./useAppState";
-export const AppStateContext = createContext()
-export const AppDispatchContext = createContext()
+export const AppStateContext = createContext();
+export const AppDispatchContext = createContext();
 
-
-function AppProvider({children}) {
-    const [state, dispatch] = useReducer(appReducer, initialState)
-    return (
-        <AppStateContext.Provider value={state}>
-            <AppDispatchContext.Provider value={dispatch}>
-                {children}
-            </AppDispatchContext.Provider>
-        </AppStateContext.Provider>
-    )
+function AppProvider({ children }) {
+  const [state, dispatch] = useReducer(appReducer, initialState);
+  return (
+    <AppStateContext.Provider value={state}>
+      <AppDispatchContext.Provider value={dispatch}>
+        {children}
+      </AppDispatchContext.Provider>
+    </AppStateContext.Provider>
+  );
 }
 
-export {AppProvider,useAppDispatch,useAppState,useAppActions}
+export { AppProvider, useAppDispatch, useAppState, useAppActions };
--- src/context/Application/useAppState.js
+++ src/context/Application/useAppState.js
@@ -1,6 +1,6 @@
-import {useContext} from 'react'
-import {AppStateContext} from "./store";
-export default function useAppState(){
-    const context=useContext(AppStateContext)
-    return context
+import { useContext } from "react";
+import { AppStateContext } from "./store";
+export default function useAppState() {
+  const context = useContext(AppStateContext);
+  return context;
 }
